d518521e6804226990c1f7d149b70425e908da0b
Anuraag <ada2161@columbia.edu>
2017-10-05 19:01:41 -0400

    all cases passing

--- src/viewservice/server.go
+++ src/viewservice/server.go
@@ -18,7 +18,7 @@ type ViewServer struct {
 
   // Your declarations here.
   //To keep the most recent communication time
-  timeMap map[string]time.Time
+  timeMap map[string]int64
   //currentView is the view which has been acknowledged by the primary
   currentView View
   //viewServiceView is the viewservices view and will become the view for the primary once the primary acknowledges it is in a view 1 behind the new one 
@@ -32,40 +32,50 @@ type ViewServer struct {
 func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
 
   // Your code here.
+  //TODO Handle locks
   //When a ping is recieved we do the following:
   //store the time it is recieved for the server by
   // Checking if its the first ping or not and update the map accordingly
-  vs.timeMap[args.Me] = time.Now()
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
+  now := time.Now()
 
+  vs.timeMap[args.Me] = now.UnixNano()
   // Doing it for the first time only and return instantly to avoid complications
   if vs.currentView.Primary == "" && vs.currentView.Viewnum == 0 {
 	 vs.currentView.Primary = args.Me
 	 vs.currentView.Viewnum = vs.currentView.Viewnum + 1 
+     vs.IsConsistent = false
 	 vs.viewServiceView = vs.currentView
-	 reply.View = vs.currentView
+	 reply.View = vs.viewServiceView
 	 return nil
   }
-  //TODO Check if it is the primary and check if it is on the correct view.
-  if vs.currentView.Primary == args.Me && vs.IsConsistent==false{
-  	vs.currentView = vs.viewServiceView
-    vs.IsConsistent = true
+
+   if vs.currentView.Primary == args.Me && args.Viewnum == 0 {
+	 vs.makeNewPrimary()
+	 // reply.View = vs.viewServiceView
+	 // return nil
+   }
+
+  // //TODO Check if not consistent and view is different 
+  if vs.currentView.Primary == args.Me && (vs.IsConsistent==false  && args.Viewnum == vs.currentView.Viewnum) {
+	  vs.IsConsistent = true
   }
-  //If backup can be assigned
-  if vs.viewServiceView.Backup == "" && vs.viewServiceView.Primary != args.Me {
-	 vs.viewServiceView.Backup = args.Me
-	 vs.viewServiceView.Viewnum = vs.viewServiceView.Viewnum + 1 
-	 if vs.IsConsistent ==true{
-	 	vs.currentView = vs.viewServiceView
-	 	vs.IsConsistent = false
-	 }
-  }
+// //If backup can be assigned
+   if vs.viewServiceView.Backup == "" && vs.viewServiceView.Primary != args.Me {
+	  vs.viewServiceView.Backup = args.Me
+	  vs.viewServiceView.Viewnum = vs.viewServiceView.Viewnum + 1 
+   }
+
+   if vs.IsConsistent == true && vs.currentView!=vs.viewServiceView{
+	   vs.currentView = vs.viewServiceView
+	   vs.IsConsistent = false
+   }
 
 
-  reply.View = vs.currentView
-  // fmt.Println(reply.View.Primary)  
-  // fmt.Println(reply.View.Primary)  
-  
-  return nil
+   reply.View = vs.currentView
+
+   return nil
 }
 
 // 
@@ -73,10 +83,41 @@ func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
 //
 func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
   // Your code here.
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
   reply.View = vs.currentView 
   return nil
 }
 
+func (vs *ViewServer) makeNewBackup() {
+	// Your code here.
+	//Backup might have failed
+	vs.viewServiceView.Backup = ""
+	for server := range vs.timeMap {
+	 	if server != vs.currentView.Primary{
+	 		vs.currentView.Backup = server
+	 		vs.IsConsistent = false
+	 		vs.viewServiceView = vs.currentView
+	 	}
+	}
+	if vs.IsConsistent == true{
+	   vs.currentView = vs.viewServiceView	
+	}
+	vs.viewServiceView.Viewnum = vs.viewServiceView.Viewnum + 1 
+	vs.IsConsistent = false
+}
+ 
+
+func (vs *ViewServer) makeNewPrimary() {
+  // Your code here.
+  //If primary fails we can skip views or we can go to the next state in which a 
+  //Backup might have failed
+  vs.currentView.Primary = vs.currentView.Backup
+  vs.currentView.Backup = ""
+  vs.currentView.Viewnum = vs.currentView.Viewnum + 1 
+  vs.IsConsistent = false
+  vs.viewServiceView = vs.currentView
+}
 
 //
 // tick() is called once per PingInterval; it should notice
@@ -84,8 +125,23 @@ func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 // accordingly. It should also update the map of available workers in the same way.
 //
 func (vs *ViewServer) tick() {
-
-  // Your code here.
+	// Your code here.
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+	for server, lastPing := range vs.timeMap {
+		currentTime := time.Now()
+		currentT := currentTime.UnixNano()
+		elapsedTimeInMilli := (currentT-lastPing) 
+		//Check if conversion works
+		if(elapsedTimeInMilli > (DeadPings * int64(PingInterval))){
+			delete(vs.timeMap, server)
+			if(server == vs.currentView.Primary && vs.IsConsistent == true){
+				vs.makeNewPrimary()
+			}else if(server == vs.currentView.Backup){
+				vs.makeNewBackup()
+			}
+		}
+	}
 }
 
 //
@@ -94,16 +150,20 @@ func (vs *ViewServer) tick() {
 // please don't change this function.
 //
 func (vs *ViewServer) Kill() {
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
   vs.dead = true
   vs.l.Close()
 }
 
 func StartServer(me string) *ViewServer {
   vs := new(ViewServer)
+  vs.mu.Lock()
+  defer vs.mu.Unlock()
   vs.me = me
   // Your vs.* initializations here.
 
-  vs.timeMap = make(map[string]time.Time)
+  vs.timeMap = make(map[string]int64)
   vs.currentView.Primary = ""
   vs.currentView.Backup = ""
   vs.currentView.Viewnum = 0
